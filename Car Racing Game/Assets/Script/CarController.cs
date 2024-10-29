c7f8a7871369dc5fccab8cb6f9b84a91ee375a41
erikokres <56141967+erikokres@users.noreply.github.com>
2024-10-29 14:16:29 +0700

    Add Main

---
+++ Car Racing Game/Assets/Script/CarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float maxSpeed;
+
+
+    public Rigidbody theRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.AddForce(new Vector3(0f, 0f, 10f));
+
+        transform.position = theRB.position;
+    }
+}
